f1603f37ae8ccee30cee567b98a5665cd78afaa0
ashish prajapat <ashishprajapat507@gmail.com>
2026-04-04 07:45:36 +0530

    setup skeletion

--- FrontEnd/src/components/skeletons/AllListingSckeleton.jsx
+++ FrontEnd/src/components/skeletons/AllListingSckeleton.jsx
@@ -2,9 +2,9 @@
 
 function AllListingSckeleton() {
     return (
-        <div className='flex w-full flex-col gap-1 '>
+        <div className='flex   w-full  flex-col gap-1 '>
 
-            <div className="skeleton h-44 w-full"></div>
+            <div className="skeleton  h-44 w-full"></div>
             <div className="skeleton h-44 w-full"></div>
             <div className="skeleton h-44 w-full"></div>
         </div>
--- FrontEnd/src/components/skeletons/HomePageSkeleton.jsx
+++ FrontEnd/src/components/skeletons/HomePageSkeleton.jsx
@@ -2,17 +2,17 @@
 
 function HomePageSkeleton() {
 
-    const homePageSkel = Array(7).fill(null);
+    const homePageSkel = Array(8).fill(null);
     return (
         <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-7 md:mx-28 my-10 gap-5" >
+            <div className="grid max-w-7xl sm:w-full   mx-auto px-2 items-center  mb-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6" >
                 {
                     homePageSkel.map((_, i) => (
-                        <div key={i} className="flex w-80  flex-col gap-4">
-                            <div className="skeleton h-32 w-full"></div>
-                            <div className="skeleton h-4 w-28"></div>
-                            <div className="skeleton h-4 w-full"></div>
-                            <div className="skeleton h-4 w-full"></div>
+                        <div key={i} className="flex lg:80  sm:w-full p-2 lg:w-80    flex-col gap-4">
+                            <div className="skeleton h-32 "></div>
+                            <div className="skeleton h-4"></div>
+                            <div className="skeleton h-4 "></div>
+                            <div className="skeleton h-4 "></div>
                         </div>
                     ))
                 }
